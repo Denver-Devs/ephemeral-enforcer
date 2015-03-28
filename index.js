682eeb892db027de6c1f3c02b516df457dff362f
Talon Poole <talon@theghostin.me>
2015-03-28 10:11:56 -0600

    style error

--- index.js
+++ index.js
@@ -48,11 +48,10 @@ router.post('/ephemeral', parseBody, function (req, res) {
   res.end(response)
 })
 
-router.all('*', function(req, res) {
+router.all('*', function (req, res) {
   log(req.method)
   res.end('Send POST requests to /ephemeral please and thanks')
 })
-
 
 var app = http.createServer(function (req, res) {
   router(req, res, finalhandler(req, res))
