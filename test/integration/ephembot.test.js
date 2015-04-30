ff3ef2fbb16a980cd98936f44b65ff010fb034c4
talon.poole@gmail.com <talon.poole@gmail.com>
2015-04-30 16:36:51 -0600

    broken nock

--- test/integration/ephembot.test.js
+++ test/integration/ephembot.test.js
@@ -1,7 +1,7 @@
-/* global describe it before after afterEach */
+/* global describe it before after beforeEach afterEach */
 
 import {expect} from 'chai'
-import fixture from '../fixture/get_history.fixture'
+import fixtures from '../fixture/get_history.fixture'
 import nock from 'nock'
 import path from 'path'
 
@@ -21,53 +21,49 @@ const res = {
 
 let request = require('supertest')(app)
 
-describe('POST /ephemeral', function () {
-  before(function () {
-    // nock.recorder.rec()
-    nock.enableNetConnect(/^.*127\.0\.0\.1.*$/)
+before(function () {
+  // nock.recorder.rec()
+  nock.enableNetConnect(/^.*127\.0\.0\.1.*$/)
+})
 
-  })
+after(function () {
+  // nock.recorder.play()
+  nock.disableNetConnect(/^.*127\.0\.0\.1.*$/)
+})
 
-  after(function () {
-    // nock.recorder.play()
-    nock.disableNetConnect(/^.*127\.0\.0\.1.*$/)
-  })
+afterEach(function () {
+  nock.cleanAll()
+})
 
-  afterEach(function () {
-    nock.cleanAll()
-  })
+describe('Integration', function () {
+  describe('POST /ephemeral', function () {
+    beforeEach(function () {
+      nock('https://slack.com')
+        .filteringPath(/latest=[^&]*/g, 'latest=XXX')
+        .get(`/api/channel.history?token=&channel_id=${CHANNEL_ID}&latest=XXX`)
+        .reply(200, fixtures[0])
 
-  it(`text=on&channel_id=${CHANNEL_ID} should turn ephembot on`, function (done) {
-    nock('https://slack.com')
-      .filteringPath(function filteringPath (path) {
-        console.log('PATH', path)
-        return path.match(/channels\.history/g)
-      })
-      .get(`/api/channels.history?token=&channel=${CHANNEL_ID}&latest=XXX`)
-      .reply(200, fixture[0])
-
-    nock('https://slack.com')
-      .post('/api/chat.delete')
-      .times(40).reply(200, res)
-
-    let body = `text=on&channel_id=${CHANNEL_ID}`
-    request
-      .post('/ephemeral')
-      .send(body)
-      .expect(200, function (err) {
-        if (err) return done(err)
-        db.findOne(CHANNEL_ID).then(function (data) {
-          expect(data).to.be.ok
-          done(err)
+        .post('/api/chat.delete').times(40).reply(200, res)
+    })
+    it('should turn ephembot on when told', function (done) {
+      let body = `text=on&channel_id=${CHANNEL_ID}`
+      request
+        .post('/ephemeral')
+        .send(body)
+        .expect(200, function (err) {
+          if (err) return done(err)
+          db.findOne(CHANNEL_ID).then(function (data) {
+            expect(data).to.be.ok
+            done(err)
+          })
         })
-      })
-  })
-
-  it('should respond 400 to unsupported commands', function (done) {
-    let body = `text=butts&channel_id=${CHANNEL_ID}`
-    request
-      .post('/ephemeral')
-      .send(body)
-      .expect(400, done)
+    })
+    it('should respond 400 to unsupported commands', function (done) {
+      let body = `text=butts&channel_id=${CHANNEL_ID}`
+      request
+        .post('/ephemeral')
+        .send(body)
+        .expect(400, done)
+    })
   })
 })
